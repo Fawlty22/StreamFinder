a0aec39afc7da79a0946c5cba3b9540700da5e43
Matt Keys <MatthewjKeys22@gmail.com>
2023-11-05 13:00:03 -0500

    tailwinds installed

---
+++ streamfinder/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#27374D',
+        'teal': '#526D82',
+        'light-gray': '#9DB2BF',
+        'lighter-gray': '#DDE6ED',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
